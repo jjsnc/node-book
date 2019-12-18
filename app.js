d1cb39d65754eca17173714f2a25cd01c445168a
Damon_care <damon_care@jjsncMacBook-Pro.local>
2019-12-18 17:18:55 +0800

    init

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const Koa = require('koa')
-
+const Router = require('koa-router')
 const app = new Koa()
+var router = new Router();
 
-app.use(async (ctx, next)=> {
+
+
+router.get('/classic/latest', (ctx, next) => {
     if(ctx.path==="/classic/latest" && ctx.method==="GET"){
         ctx.body = {key:'666'}
     }
-})
+  });
 
+
+// app.use(async (ctx, next)=> {
+//     if(ctx.path==="/classic/latest" && ctx.method==="GET"){
+//         ctx.body = {key:'666'}
+//     }
+// })
+
+app.use(router.routes())
 app.listen(3000)
