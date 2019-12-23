9ba2978599f2e5c0db045bb59d28a712d59ffab6
Damon <zhuowangyi@163.com>
2019-12-23 14:37:05 +0800

    init

--- middlewares/exception.js
+++ middlewares/exception.js
@@ -1,44 +1,36 @@
 const {HttpException} = require('../core/http-exception')
 
-
-const catchError = async (ctx, next) => {
+const catchError = async (ctx, next)=>{
     try {
         await next()
     } catch (error) {
-        ctx.body = {
-            code: error.error_code,
-            url: error.request_url,
-            message: error.message
-        }
-        ctx.status = error.status
         // 开发环境
         // 生产环境
         // 开发环境 不是HttpException
-            const isHttpException = error instanceof HttpException
-            // const isDev = global.config.environment === 'dev'
-             const isDev = false
-            if(isDev && !isHttpException){
-                throw error                               
+        const isHttpException = error instanceof HttpException
+        // const isDev = global.config.environment === 'dev'
+        
+        // if(isDev && !isHttpException){
+        //     throw error
+        // }
+        
+        if(isHttpException){
+            ctx.body = {
+                msg:error.msg,
+                error_code:error.errorCode,
+                request:`${ctx.method} ${ctx.path}`
             }
-
-            if(isHttpException){
-                ctx.body = {
-                    msg:error.msg,
-                    error_code:error.errorCode,
-                    request:`${ctx.method} ${ctx.path}`
-                }
-                ctx.status = error.code
+            ctx.status = error.code
+        }
+        else{
+            ctx.body = {
+                msg: 'we made a mistake O(∩_∩)O~~',
+                error_code: 999,
+                request:`${ctx.method} ${ctx.path}`
             }
-            else{
-                ctx.body = {
-                    msg: 'we made a mistake O(∩_∩)O~~',
-                    error_code: 999,
-                    request:`${ctx.method} ${ctx.path}`
-                }
-                ctx.status = 500
-            }
+            ctx.status = 500
+        }
     }
 }
-
 
 module.exports = catchError
