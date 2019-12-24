73a4cb9e4b21572512e11dec6e045464a7c17af9
Damon <zhuowangyi@163.com>
2019-12-24 10:29:55 +0800

    sequelize init

--- core/db.js
+++ core/db.js
@@ -17,20 +17,20 @@ const sequelize = new Sequelize(dbName,user,password,{
     timezone: '+08:00',
     define:{
         //create_time  update_time delete_time
-        timestamps:true,
-        paranoid:true,
-        createdAt:'created_at',
-        updatedAt:'updated_at',
-        deletedAt:'deleted_at',
-        underscored:true,
-        freezeTableName:true,
-        scopes:{
-            bh:{
-                attributes:{
-                    exclude:['updated_at','deleted_at','created_at']
-                }
-            }
-        }
+        // timestamps:true,
+        // paranoid:true,
+        // createdAt:'created_at',
+        // updatedAt:'updated_at',
+        // deletedAt:'deleted_at',
+        // underscored:true,
+        // freezeTableName:true,
+        // scopes:{
+        //     bh:{
+        //         attributes:{
+        //             exclude:['updated_at','deleted_at','created_at']
+        //         }
+        //     }
+        // }
     }
 })
 
