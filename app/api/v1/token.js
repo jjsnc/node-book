fbb507fee81884ff4484ba6db417975c4d52e92f
Damon <zhuowangyi@163.com>
2019-12-25 16:18:16 +0800

    验证用户名和密码

--- app/api/v1/token.js
+++ app/api/v1/token.js
@@ -12,33 +12,32 @@ const {
 
 // const { WXManager } = require('../../services/wx')
 
-// const { generateToken } = require('../../../core/util')
+const { generateToken } = require('../../../core/util')
 // const {Auth} = require('../../../middlewares/auth')
 
 const router = new Router({
     prefix: '/v1/token'
 })
 
-// router.post('/', async (ctx) => {
-//     const v = await new TokenValidator().validate(ctx)
-//     let token;
-//     switch (v.get('body.type')) {
-//         case LoginType.USER_EMAIL:
-//             token = await emailLogin(v.get('body.account'),
-//                 v.get('body.secret'))
-//             break
-//         case LoginType.USER_MINI_PROGRAM:
-//             token = await WXManager.codeToToken(v.get('body.account'))
-//             break
-//         case LoginType.ADMIN_EMAIL:
-//             break
-//         default:
-//             throw new global.errs.ParameterException('没有相应的处理函数')
-//     }
-//     ctx.body = {
-//         token
-//     }
-// })
+router.post('/', async (ctx) => {
+    const v = await new TokenValidator().validate(ctx)
+    let token;
+    switch (v.get('body.type')) {
+        case LoginType.USER_EMAIL:
+            token = await emailLogin(v.get('body.account'),v.get('body.secret'))
+            break
+        case LoginType.USER_MINI_PROGRAM:
+            // token = await WXManager.codeToToken(v.get('body.account'))
+            break
+        case LoginType.ADMIN_EMAIL:
+            break
+        default:
+            throw new global.errs.ParameterException('没有相应的处理函数')
+    }
+    // ctx.body = {
+    //     token
+    // }
+})
 
 // router.post('/verify', async (ctx)=>{
 //     // token
@@ -49,10 +48,10 @@ const router = new Router({
 //     }
 // })
 
-// async function emailLogin(account, secret) {
-//     const user = await
-//         User.verifyEmailPassword(account, secret)
-//     return token = generateToken(user.id, Auth.USER)
-// }
+async function emailLogin(account, secret) {
+    const user = await  User.verifyEmailPassword(account, secret)
+    console.log(user,'user')
+    // return token = generateToken(user.id, Auth.USER)
+}
 
 module.exports = router
