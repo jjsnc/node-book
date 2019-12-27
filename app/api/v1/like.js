0d6f00147666328067f2f7dea01d0f90197ad8a3
Damon <zhuowangyi@163.com>
2019-12-27 18:03:30 +0800

    init

--- app/api/v1/like.js
+++ app/api/v1/like.js
@@ -2,9 +2,9 @@ const Router = require('koa-router')
 const {
     LikeValidator
 } = require('../../validators/validator')
-// const {
-//     Favor
-// } = require('../../models/favor')
+const {
+    Favor
+} = require('../../models/favor')
 
 const {success} = require('../../lib/helper')
 
@@ -17,21 +17,23 @@ const router = new Router({
 })
 
 router.post('/', new Auth().m, async ctx => {
-    console.log('2222333')
-    // const v = await new LikeValidator().validate(ctx,{
-    //     id:'art_id'
-    // })
-    // await Favor.like( v.get('body.art_id'), v.get('body.type'),ctx.auth.uid)
-    // success()
+    const v = await new LikeValidator().validate(ctx,{
+        id:'art_id'
+    })
+    ctx.body = {
+        v
+    }
+    await Favor.like( v.get('body.art_id'), v.get('body.type'),ctx.auth.uid)
+    success()
 })
 
-// router.post('/cancel', new Auth().m, async ctx => {
-//     const v = await new LikeValidator().validate(ctx,{
-//         id:'art_id'
-//     })
-//     await Favor.disLike(v.get('body.art_id'), v.get('body.type'), ctx.auth.uid )
-//     success()
-// })
+router.post('/cancel', new Auth().m, async ctx => {
+    const v = await new LikeValidator().validate(ctx,{
+        id:'art_id'
+    })
+    await Favor.disLike(v.get('body.art_id'), v.get('body.type'), ctx.auth.uid )
+    success()
+})
 
 module.exports = router
 
