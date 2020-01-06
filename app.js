cc924319fa63ab80a8e955424f510e7cdfaba6ac
Damon <zhuowangyi@163.com>
2020-01-06 14:48:14 +0800

    koa 静态资源

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('module-alias/register')
 const Koa = require('koa')
 
 const parser = require('koa-bodyparser')
-
+const path = require('path')
+const static = require('koa-static')
 const catchError = require('./middlewares/exception')
 const {
     User
@@ -12,7 +13,7 @@ app.use(catchError)
 app.use(parser())
 
 const InitManager = require('./core/init')
-
+app.use(static(path.join(__dirname,'./static')))
 InitManager.initCore(app)
 
 app.listen(3000)
